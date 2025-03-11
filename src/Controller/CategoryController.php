accbf816a07de5e4684b43afb4bd9993471522c2
Alban Igor <agbotaalban@gmail.com>
2025-03-11 15:40:51 +0100

    TP partir 5:Ajouter, Afficher, Modifier et Supprimer les categories

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Category;
+use App\Form\CategoryFormType;
+use App\Repository\CategoryRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class CategoryController extends AbstractController
+{
+    ///Cette fonction permet d'afficher les elements de la db
+    #[Route('/category', name: 'app_category')]
+    public function index(CategoryRepository $categoryRepository): Response
+    {
+        $categories = $categoryRepository->findAll(); //Cette ligne permet de recupérer toutes les categories de db
+        //dd($categories);
+        return $this->render('category/index.html.twig', [
+            'categories' => $categories,
+        ]);
+    }
+
+    // Cette fonction permet d'enrégistrer un objet dans la db
+    #[Route('/category/new', name: 'app_category_new')]
+    public function addCategory(EntityManagerInterface $entityManager, Request $request): Response
+    {
+        $category = new Category();
+        $form = $this->createForm(CategoryFormType::class, $category);
+        // permet de recuperer le contenu de la requette
+        $form->handleRequest($request);
+
+        //Vérifier si le formuler a été soumis et valide
+        if ($form->isSubmitted() && $form->isValid()) {
+            //dd($category);
+            $entityManager->persist($category);
+            $entityManager->flush();
+            return $this->redirectToRoute('app_category');
+        }
+
+        return $this->render('category/new.html.twig', ['form' => $form->createView()]);
+    }
+
+
+    ///Permet de recupérer et de mettre a jours un objet de la db
+    #[Route('/category/{id}/update', name: 'app_category_update')]
+    public function updateCategorie(Category $category, EntityManagerInterface $entityManager, Request $request): Response
+    {
+        $form = $this->createForm(CategoryFormType::class, $category);
+        // permet de recuperer le contenu de la requette
+        $form->handleRequest($request);
+
+        //Vérifier si le formuler a été soumis et valide
+        if ($form->isSubmitted() && $form->isValid()) {
+            //dd($category);
+            $entityManager->flush();
+            return $this->redirectToRoute('app_category');
+        }
+
+        return $this->render('category/update.html.twig', ['form' => $form->createView()]);
+    }
+
+    ///Permet de recupérer et de supprimer un objet de la db
+    #[Route('/category/{id}/delete', name: 'app_category_delete')]
+    public function deleteCategorie(Category $category, EntityManagerInterface $entityManager, Request $request): Response
+    {
+        $entityManager->remove($category);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_category');
+    }
+}
