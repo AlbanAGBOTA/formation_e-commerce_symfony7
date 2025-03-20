99faee51c7a9c5d4b9af9076f4b0f9a43f4e0f60
Alban Igor <agbotaalban@gmail.com>
2025-03-20 16:54:51 +0100

    part16 | TP - Site e-commerce. Afficher et et supprimer le panier

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -12,7 +12,7 @@ final class CartController extends AbstractController
 {
     public function __construct(private readonly ProductRepository $productRepository) {}
 
-    //Cette route permette de recuperer le panier
+    //Cette route permette de recuperer les produits de la panier
     #[Route('/cart', name: 'app_cart', methods: ['GET'])]
     public function index(SessionInterface $session): Response
     {
@@ -29,7 +29,7 @@ final class CartController extends AbstractController
         $total = array_sum(array_map(function ($item) {
             return $item['product']->getPrice() * $item['quantity'];
         }, $cartWhitData));
-        dd($total);
+        //dd($total);
 
         return $this->render('cart/index.html.twig', [
             'items' => $cartWhitData,
@@ -37,7 +37,7 @@ final class CartController extends AbstractController
         ]);
     }
 
-    //Cette fonction permette d'ajouter les produits dans le panier
+    //Cette fonction permette d'ajouter un produit au panier
     #[Route('/cart/add/{id}', name: 'app_cart_new', methods: ['GET'])]
     public function addToCart(int $id, SessionInterface $session): Response
     {
@@ -48,6 +48,28 @@ final class CartController extends AbstractController
             $cart[$id] = 1;
         }
         $session->set('cart', $cart);
+        return $this->redirectToRoute('app_cart');
+    }
+
+    //Cette fonction permette de supprimer les elements du panier panier
+    #[Route('/cart/remove/{id}', name: 'app_cart_product_remove', methods: ['GET'])]
+    public function removeToCart($id, SessionInterface $session): Response
+    {
+
+        $cart = $session->get('cart', []);
+        if (!empty($cart[$id])) {
+            unset($cart[$id]);
+        }
+        $session->set('cart', $cart);
+
+        return $this->redirectToRoute('app_cart');
+    }
+
+    //Cette fonction permette de supprimer le panier
+    #[Route('/cart/remove', name: 'app_cart_remove', methods: ['GET'])]
+    public function remove(SessionInterface $session): Response
+    {
+        $session->set('cart', []);
         return $this->redirectToRoute('app_cart');
     }
 }
